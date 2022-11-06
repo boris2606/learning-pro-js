aace7e89088e8b4c3a4bcfcad6c5e10d7a5fd96a
Boris <boris260692@gmail.com>
2022-11-06 11:20:36 +0200

    Added new block

--- src/user.js
+++ src/user.js
@@ -3,22 +3,8 @@
 let listBlock = document.querySelectorAll('.fadeBlock')
 function showWithDelay(listBlock) {
     for (let i = 0; i < listBlock.length; i++) {
-        setTimeout(() => { listBlock[i].classList.add('fadeIn')}, 1 * i);
+        setTimeout(() => { listBlock[i].classList.add('fadeIn')}, 200 * i);
     }
 }
 showWithDelay(listBlock);
 // Завершення коду появи блоків
-
-let person = {
-    name: 'Boris',
-    age: 30,
-    country: 'Ukrain',
-    city: 'Kiev'
-}
-let {name,age,...adress} = person
-
-
-function logPerson ({name, age}){
-    console.log(name + ' ' + age);
-}
-logPerson(person)
